57f670b123bc5a02acb78d97e4c4656d5fcd842f
Jonathan Velazquez <e.jonathan.velazquez@gmail.com>
2022-12-09 11:32:49 -0800

    Se agrega numero de telefono en whatsapp

--- resources/views/components/whatsapp-button.blade.php
+++ resources/views/components/whatsapp-button.blade.php
@@ -1,6 +1,5 @@
 <!-- We need a whatsapp button on the bottom right corner -->
 <div class="whatsapp">
-    <a href="https://wa.me/521664-123-4567" target="_blank">
-        
+    <a href="https://wa.me/+526161112357?text=Hola,%20me%20gustaría%20más%20información%20sobre%20los%20terrenos%20en%20venta%20en%20Camino%20Viejo%20Residencial" target="_blank">
     </a>
 </div>
